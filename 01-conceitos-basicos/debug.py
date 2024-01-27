0dfba6683a6b8893780152546634af9f9f0c5da7
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-01-27 11:54:13 -0300

    Primeiros arquivos propostos pelo livro

---
+++ 01-conceitos-basicos/debug.py
@@ -0,0 +1,10 @@
+#Python e Django: Desenvolvimento Web Moderno e ágil
+
+print('Usuário: Francisco Maciel')
+print('Aniversário: 04 de outubro')
+idade=42
+serie="Game of Thrones"
+livro="Odisseia"
+print('Idade', idade,'anos')
+print('Série favorita:', serie)
+print('Livro favorito:', livro)
---
+++ 01-conceitos-basicos/volume.py
@@ -0,0 +1,6 @@
+altura = 1.5
+largura = 5.0
+comprimento = 6.0
+area_base = largura * comprimento
+volume = altura * area_base
+print('O volume em m3 é', volume)
