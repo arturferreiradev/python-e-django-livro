022e07076c9c57006b199dedabf6618d794c001b
Artur Ferreira <148302034+arturferreiradev@users.noreply.github.com>
2024-01-28 12:57:07 -0300

    Exemplos de formatação

---
+++ 01-conceitos-basicos/formatacao_strings.py
@@ -0,0 +1,13 @@
+print(‘Hoje é dia %d’ % 15)
+
+print(‘12 em octal é %o’ % 12)
+
+print(‘255 em hexadecimal é %x’ % 255)
+
+print(‘pi com duas casas decimais é aproximadamente %.2f’ % 3.1415926)
+
+print(‘pi com tres casas decimais é aproximadamente %.3f’ % 3.1415926)
+
+nome = ‘Francisco’
+
+print(‘Meu nome é %s’ % nome)
