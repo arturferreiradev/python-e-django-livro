415d21551cc930ef9488d08ad6957d2c11467c66
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-01-29 11:30:40 -0300

    Exemplos utilizando print

---
+++ 01-conceitos-basicos/print.py
@@ -0,0 +1,10 @@
+nome='Francisco'
+print(f'Nome do usuário: {nome}') #Formatted String Litterals
+print("Constantes - pi: %.2f, e: %.2f" % (3.1415926,2.7118281)) 
+print(f'A média aritmética simples entre 2 e 7 é {(2+7)/2}')
+print('Ser ou não ser,',end='') # Imprimir na mesma linha
+print('eis a questão.') 
+print("um", "dois", "três", 4, sep="*") # Separador entre os valores
+print("um", "\n", "dois", "\n", "três", "\n", sep="") # O python define um separador para listas padrão, um espaço e para ser retirado deve ser definido um separador sem esse espaço
+
+
