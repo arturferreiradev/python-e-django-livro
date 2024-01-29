5141ce0a317f8bb952036dc889d1fa4d797a08cc
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-01-29 15:05:30 -0300

    Exercícios resolvidos

---
+++ 01-conceitos-basicos/exercicios_resolvidos_01.py
@@ -0,0 +1,22 @@
+# Uma forma de arte que ficou popular por algum tempo quando os computadores começaram a aparecer no 
+#dia a dia das pessoas foi a chamada ASCII Art. ASCII (pronuncia-se “asquí”) é a sigla de American 
+#Standard Code for Information Interchange — uma das primeiras codificações de caracteres usadas.
+#Aquele tipo de arte era feito até mesmo em máquinas de escrever e consistia em figuras compostas 
+#apenas por caracteres disponíveis na tabela de página de código dos computadores. Escreva um 
+#programa para gerar uma imagem simplificada ASCII Art de uma árvore de Natal.
+
+print('####+####')
+
+print('###+++###')
+
+print('##+++++##')
+
+print('#+++++++#')
+
+print('#0#||#0##')
+
+print('###||####')
+
+print('###||####')
+
+print('=========')
