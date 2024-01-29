1d09958073674d15bfec25e342325ba2db2391ca
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-01-29 15:55:13 -0300

    Resolução exercícios propostos

---
+++ 01-conceitos-basicos/exericicios_propostos_01.py
@@ -0,0 +1,3 @@
+# Lembrando da época do “jardim de infância”, imagine que um garoto chamado Joãozinho fez alguma travessura e, como castigo, a professora mandou-o escrever dez vezes no quadro “Não vou mais atrapalhar a aula”. Como os tempos mudaram, crie um programa que imprima a referida frase dez vezes para ajudar o Joãozinho.
+
+print("jardim de infância\n" * 10)
