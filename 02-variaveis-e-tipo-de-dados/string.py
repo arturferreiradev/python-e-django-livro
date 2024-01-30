469e49d045eb537f6f1d2f733292a864d58b6266
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-01-30 12:15:33 -0300

    Exemplos usando string

---
+++ 02-variaveis-e-tipo-de-dados/string.py
@@ -0,0 +1,15 @@
+# Strings a cadeia de caracteres, o python diferente de outras linguagens é zero-based, ou seja, por definição o índice é 0.
+
+saudacao = "Hello World!"
+
+print(saudacao)
+
+print(saudacao[0])
+
+print(saudacao[7])
+
+# Substring é um pedaço de uma outra string
+print(saudacao[0:5])
+
+# Concatenação de strings
+print("Brasil"+"eiro")
