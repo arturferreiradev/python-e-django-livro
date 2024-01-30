7f2b3abdb0f4d857c92764fac7045a1c9b2532c0
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-01-30 14:41:45 -0300

    Exemplos de tuplas

---
+++ 02-variaveis-e-tipo-de-dados/tuplas.py
@@ -0,0 +1,7 @@
+# Tuplas são estruturas de dados invariáveis
+
+tupla_frutas = ('laranja', 'mamão', 'abacaxi', 'melão') # após criado uma tupla seus dados não podem ser alterados ou removidos, porém é possível trocar uma tupla inteira por outra
+
+print(tupla_frutas)
+
+print(tupla_frutas[1])
