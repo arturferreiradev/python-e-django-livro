f1da23458f5b46cb305ef41e133a114db232a90e
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-01-30 12:23:26 -0300

    Exemplos de tipos numericos em Python

---
+++ 02-variaveis-e-tipo-de-dados/tipos_numericos.py
@@ -0,0 +1,17 @@
+# Ao contrário de outras linguagens o Python utiliza poucos tipos para representar números
+# São eles: 
+quantidade_ingressos = 1000 #int
+
+pi = 3.1415926 # float
+
+c1 = (-3 + 10j) # complex
+
+c2 = (7 - 2j) # complex
+
+c3 = (1 + 0j) # complex
+
+print(c1.real) # retorna o valor real de c1
+
+print(c2.imag) # retorna a parte imaginária de c2
+
+print(c3.conjugate()) # Retorna o conjugado de c3
