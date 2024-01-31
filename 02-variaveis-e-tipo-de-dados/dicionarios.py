4c33d3faf0d86d686324b76a717e655b7d4693ce
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-01-30 21:33:50 -0300

    Exemplos de dicionário

---
+++ 02-variaveis-e-tipo-de-dados/dicionarios.py
@@ -0,0 +1,6 @@
+# Um dicionário é um tipo de dados que agrega elementos e lhes atribui "etiquetas" no lugar de índices, chaves de acesso para os valores. Dicionários são do tipo variáveis e podem ser alterados.
+
+contato = {"id": 10, "nome": "Francisco", "fone:": "(99)99999-9999" } # As chaves e os valores podem ser de qualquer tipo, porém a chave geralmente será do tipo string
+print(contato) 
+
+print(contato["id"]) # Acessando valores do dicionário através da chave
