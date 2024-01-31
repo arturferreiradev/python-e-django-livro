f4fa199979b2dd723baec3e01994ac4e8cabb061
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-01-31 14:56:52 -0300

    Resolução exercicio proposto

---
+++ 03-estruturas-condicionais/exercicios_propostos_02.py
@@ -0,0 +1,14 @@
+# Crie um programa que solicitará ao usuário as medidas de três lados de um triângulo e informará como saída se os valores formam um triângulo retângulo. Os lados devem ser fornecidos em qualquer ordem (ou seja, não “vale” escrever “digite o tamanho da hipotenusa”, “digite o tamanho do cateto 1” etc.). Dica: use o Teorema de Pitágoras (“O quadrado da hipotenusa é igual à soma dos quadrados dos catetos”).
+
+lado_1 = float(input('Digite o tamanho de um dos lados do triângulo: '))
+lado_2 = float(input('Digite o tamanho de um dos lados do triângulo: '))
+lado_3 = float(input('Digite o tamanho de um dos lados do triângulo: '))
+
+if((lado_1 + lado_2) ** 2 == lado_3 ** 2):
+    print("Os lados informados formam um triângulo retângulo")
+elif((lado_2 + lado_3) ** 2 == lado_1 ** 2):
+    print("Os lados informados formam um triângulo retângulo")
+elif((lado_3 + lado_1) ** 2 == lado_2 ** 2):
+    print("Os lados informados formam um triângulo retângulo")
+else:
+    print("Os lados informados não formam um triângulo retângulo")
