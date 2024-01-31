f54b0eab1cf7e4ac57a7042d68daa22f7ac57460
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-01-31 10:21:10 -0300

    Exemplo utilizando operador ternário

---
+++ 03-estruturas-condicionais/operador_ternario.py
@@ -0,0 +1,4 @@
+# Operador ternario é uma instrução if condensada em uma linha, porém deve ser evitado pois é contrário a uma das "regras" do Zen do Python: explícito é melhor que implícito
+
+idade = 20
+print('menor de idade' if idade < 18 else 'maior de idade')
