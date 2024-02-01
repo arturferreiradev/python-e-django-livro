05ec14926a3bcc43945e48d96bd071bed90e15f9
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-01-31 23:19:56 -0300

    Exemplo usando o loop for

---
+++ 04-estruturas-de-repeticao/loop_for_2.py
@@ -0,0 +1,4 @@
+# Para iterar um número predeterminado de vezes, se quiser repetir um suíte um número específico de vezes, combine um loop for com a função range().
+
+for x in range(20):
+    print('Aprendendo Python do jeito simples')
