e12e6389a0152c5569e0cf963fc9dfb7ed0aa7bf
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-01-31 21:30:17 -0300

    Sequência de Fibonacci

---
+++ 04-estruturas-de-repeticao/fibonacci.py
@@ -0,0 +1,15 @@
+print('Sequência de Fibonacci')
+
+n = int(input('Quantos números da sequência de Fibonacci você quer ver? '))
+
+contador = 0
+x1 = 1
+x2 = 1
+fib_n = 0
+
+while(contador <= n):
+    contador = contador + 1
+    fib_n = x1 + x2
+    print(f'{x1}')
+    x1 = x2
+    x2 = fib_n
