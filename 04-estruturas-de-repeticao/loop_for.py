446769226e4b9493d611b25d1ad480b7e21294e6
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-01-31 23:19:49 -0300

    Exemplo usando o loop for

---
+++ 04-estruturas-de-repeticao/loop_for.py
@@ -0,0 +1,8 @@
+# Loops for podem ser usados para iterar sobre coleções (como listas, tuplas etc.), strings e vários outros tipos de dados um número fixo de vezes, executando uma suíte repetidas vezes a cada passada.
+
+# Iterando o for sobre uma coleção(lista, tupla etc.)
+
+alunos = ['Alice','Bob', 'Carl', 'Daniele']
+
+for aluno_atual in alunos:
+    print('O/A aluno(a) atualmente examinado é %s' % aluno_atual)
