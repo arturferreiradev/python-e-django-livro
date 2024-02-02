7a7959623d7a1ef6ce7850f0bfeb6f0935cee952
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-02-01 21:25:13 -0300

    Melhorando o codigo loops aninhados

---
+++ 04-estruturas-de-repeticao/loops_aninhados_02.py
@@ -0,0 +1,8 @@
+# Deixando o código loops_aninhados.py mais legível
+
+camisetas = ['azul', 'vermelha', 'amarela']
+shorts = ['branco', 'preto']
+
+for cor_camiseta in camisetas:
+    for cor_short in shorts:
+        print(f'Voce pode combinar uma camiseta {cor_camiseta} com um {cor_short}')
