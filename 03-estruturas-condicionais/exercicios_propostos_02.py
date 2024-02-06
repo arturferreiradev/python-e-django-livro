e327f29f4848150a428f32602fede3541b6a52ae
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-02-05 23:42:23 -0300

    Exericios propostos

--- 03-estruturas-condicionais/exercicios_propostos_02.py
+++ 03-estruturas-condicionais/exercicios_propostos_02.py
@@ -4,11 +4,11 @@ lado_1 = float(input('Digite o tamanho de um dos lados do triângulo: '))
 lado_2 = float(input('Digite o tamanho de um dos lados do triângulo: '))
 lado_3 = float(input('Digite o tamanho de um dos lados do triângulo: '))
 
-if((lado_1 + lado_2) ** 2 == lado_3 ** 2):
+if((lado_1 ** 2 + lado_2 ** 2)  == lado_3 ** 2):
     print("Os lados informados formam um triângulo retângulo")
-elif((lado_2 + lado_3) ** 2 == lado_1 ** 2):
+elif((lado_2 ** 2 + lado_3 ** 2) ** 2 == lado_1 ** 2):
     print("Os lados informados formam um triângulo retângulo")
-elif((lado_3 + lado_1) ** 2 == lado_2 ** 2):
+elif((lado_3 ** 2 + lado_1** 2 ) ** 2 == lado_2 ** 2):
     print("Os lados informados formam um triângulo retângulo")
 else:
     print("Os lados informados não formam um triângulo retângulo")
