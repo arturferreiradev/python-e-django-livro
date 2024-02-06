8738d782571096f9235a579a6f337028e9b1350b
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-02-05 22:59:43 -0300

    Exemplos de funções

---
+++ 05-funcoes-matematicas/exemplos-funcoes.py
@@ -0,0 +1,6 @@
+import math
+
+math.ceil(5.2646464) # "Função Teto" para arrendondar para o próximo número inteiro
+math.floor(5.2646464) # "Função Piso" para arredondar para o número inteiro anterior 
+
+
