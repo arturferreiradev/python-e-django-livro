a33e41cc991cdd66c55ca75fe7a1397754098603
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-02-06 23:47:55 -0300

    Função split

---
+++ 06-funcoes-que-operam-sobre-strings/split.py
@@ -0,0 +1,9 @@
+# A linha seguinte poderia ter sido lida a partir de um arquivo:
+
+linha = '7.0,3.2,4.7,1.4,versicolor'
+
+dados = linha.split(',')
+
+print('Linha original: ', linha)
+
+print('Dados extraidos com split:', dados)
