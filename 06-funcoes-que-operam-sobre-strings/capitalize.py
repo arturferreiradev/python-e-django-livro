58296a9fba0028411f661fd74df09f1095b92d4c
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-02-06 23:25:41 -0300

    Função capitalize

---
+++ 06-funcoes-que-operam-sobre-strings/capitalize.py
@@ -0,0 +1,7 @@
+# A função capitalize() recebe uma string e formata-a de modo que a primeira letra fique maiúscula e todas as demais, minúsculas. Seu uso segue o mesmo formato das outras funções já vistas.
+
+print(str.capitalize('AeIoU'))
+
+print('aEiOu'.capitalize())
+
+# Na primeira linha, a chamada foi realizada a partir do nome da classe (str); na segunda, diretamente a partir de um objeto dessa classe, no caso, uma string. Os resultados são idênticos.
